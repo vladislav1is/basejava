d82ad2aae1ada4fdf1c8e626220e774c026ba228
Vladislav Lisin <v1adis1av.lis@mail.ru>
2020-08-27 12:17:49 +0300

    Fix size search for ArrayStorage.java

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,21 +3,21 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size;
 
     void clear() {
-        int size = size();
         for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
+        size = 0;
     }
 
     void save(Resume r) {
-        int index = size();
-        storage[index] = r;
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
-        int size = size();
         Resume resume = new Resume();
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
@@ -29,12 +29,12 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        int size = size();
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 for (int j = i; j < size; j++) {
-                    storage[j] = storage[j+1];
+                    storage[j] = storage[j + 1];
                 }
+                size--;
                 break;
             }
         }
@@ -44,7 +44,6 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int size = size();
         Resume[] filledStorage = new Resume[size];
         for (int i = 0; i < size; i++) {
             filledStorage[i] = storage[i];
@@ -53,13 +52,6 @@ public class ArrayStorage {
     }
 
     int size() {
-        int size = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null) {
-                break;
-            }
-            size++;
-        }
         return size;
     }
 }
