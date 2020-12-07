cd5a189b6e0a4d646d41e3027147cbd9557576f6
Vladislav Lisin <v1adis1av.lis@mail.ru>
2020-12-08 01:40:01 +0300

    code refactoring

--- src/com/redfox/webapp/DeadLockMain.java
+++ src/com/redfox/webapp/DeadLockMain.java
@@ -5,29 +5,21 @@ public class DeadLockMain {
     private static final String lock2 = "lock2";
 
     public static void main(String[] args) {
-        Thread t1 = new Thread(() -> {
-            while (true) {
-                holder(lock1, lock2);
-            }
-        });
-        Thread t2 = new Thread(() -> {
-            while (true) {
-                holder(lock2, lock1);
-            }
-        });
-
-        t1.start();
-        t2.start();
+        holder(lock1, lock2);
+        holder(lock2, lock1);
     }
 
     static void holder(String lock1, String lock2) {
-        System.out.println(Thread.currentThread().getName() + " Waiting " + lock1);
-        synchronized (lock1) {
-            System.out.println(Thread.currentThread().getName() + " Holding " + lock1);
-            System.out.println(Thread.currentThread().getName() + " Waiting " + lock2);
-            synchronized (lock2) {
-                System.out.println(Thread.currentThread().getName() + " Holding " + lock2);
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + " Waiting " + lock1);
+            synchronized (lock1) {
+                System.out.println(Thread.currentThread().getName() + " Holding " + lock1);
+                Thread.yield();
+                System.out.println(Thread.currentThread().getName() + " Waiting " + lock2);
+                synchronized (lock2) {
+                    System.out.println(Thread.currentThread().getName() + " Holding " + lock2);
+                }
             }
-        }
+        }).start();
     }
 }
--- src/com/redfox/webapp/MainConcurrency.java
+++ src/com/redfox/webapp/MainConcurrency.java
@@ -1,14 +1,29 @@
 package com.redfox.webapp;
 
-import com.redfox.webapp.util.LazySingleton;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class MainConcurrency {
-    public static final int THREADS_NUMBER = 10_000;
-    private static int counter;
-    private static final Object LOCK = new Object();
+    private static final int THREADS_NUMBER = 10_000;
+    private int counter;
+    private final AtomicInteger atomicCounter = new AtomicInteger();
+
+    //    private static final Object LOCK = new Object();
+//    private static final Lock lock = new ReentrantLock();
+    private static final ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
+    private static final Lock WRITE_LOCK = reentrantReadWriteLock.writeLock();
+    private static final Lock READ_LOCK = reentrantReadWriteLock.readLock();
+    private static final ThreadLocal<SimpleDateFormat> DATE_FORMAT = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        }
+    };
+//    private static final ThreadLocal<DateTimeFormatter> DATE_FORMAT = ThreadLocal.withInitial(() -> DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss"));
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
@@ -30,7 +45,7 @@ public class MainConcurrency {
 
             private void inc() {
                 synchronized (this) {
-                    counter++;
+//                    counter++;
                 }
             }
         }
@@ -40,16 +55,29 @@ public class MainConcurrency {
 
         // All threads will stay in queue to obj mainConcurrency
         final MainConcurrency mainConcurrency = new MainConcurrency();
-        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+        CountDownLatch latch = new CountDownLatch(THREADS_NUMBER);
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+//        CompletionService completionService = new ExecutorCompletionService(executorService);
+//
+//        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+
         for (int i = 0; i < THREADS_NUMBER; i++) {
-            Thread thread = new Thread(() -> {
+            Future<Integer> future = executorService.submit(() ->
+//            Thread thread = new Thread(() ->
+            {
                 for (int j = 0; j < 100; j++) {
                     mainConcurrency.inc();
+                    System.out.println(DATE_FORMAT.get().format(new Date()));
+//                    System.out.println(LocalDate.now().format(DATE_FORMAT));
                 }
+                latch.countDown();
+                return 5;
             });
-            thread.start();
-            threads.add(thread);
+//            thread.start();
+//            threads.add(thread);
         }
+
+/*
         threads.forEach(t -> {
             try {
                 t.join();
@@ -57,26 +85,46 @@ public class MainConcurrency {
                 e.printStackTrace();
             }
         });
-//        Thread.sleep(500); // bad practice
-        System.out.println(counter);
-        LazySingleton.getInstance();
+*/
+        latch.await(10, TimeUnit.SECONDS);
+        executorService.shutdown();
+//        System.out.println(mainConcurrency.counter);
+        System.out.println(mainConcurrency.atomicCounter);
     }
 
-    private synchronized void inc() {
-        counter++;
-//        // use current object as monitor
-//        synchronized (this) {
-//            wait(); // use if you want pause thread work and unblock method for another thread
-//            notify(); // use if you want wake up waiting thread
-//            counter++;
-//        }
-//        // use current class as monitor, use with static methods
-//        synchronized (MainConcurrency.class) {
-//            counter++;
-//        }
-//        // use obj LOCK as monitor
-//        synchronized (LOCK) {
-//            counter++;
-//        }
+    // use synchronized in method name then current obj will be your monitor
+    private void inc() {
+        atomicCounter.incrementAndGet();
+/*
+        lock.lock();
+        try {
+            counter++;
+        } finally {
+            lock.unlock();
+        }
+*/
+
+/*
+        // use current object as monitor
+        synchronized (this) {
+            wait(); // use if you want pause thread work and unblock method for another thread
+            notify(); // use if you want wake up waiting thread
+            counter++;
+        }
+*/
+
+/*
+        // use current class as monitor, use with static methods
+        synchronized (MainConcurrency.class) {
+            counter++;
+        }
+ */
+
+/*
+        // use obj LOCK as monitor
+        synchronized (LOCK) {
+            counter++;
+        }
+ */
     }
 }
