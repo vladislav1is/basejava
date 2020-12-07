f8352ed0a53ae1aa80a0d25ccf12c8c7e05bf62c
Vladislav Lisin <v1adis1av.lis@mail.ru>
2020-12-07 15:28:43 +0300

    code refactoring

--- src/com/redfox/webapp/DeadLockMain.java
+++ src/com/redfox/webapp/DeadLockMain.java
@@ -1,32 +1,33 @@
 package com.redfox.webapp;
 
 public class DeadLockMain {
-    private static Object lock1 = new Object();
-    private static Object lock2 = new Object();
+    private static final String lock1 = "lock1";
+    private static final String lock2 = "lock2";
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Thread t1 = new Thread(() -> {
             while (true) {
-                synchronized (lock1) {
-                    System.out.println("lock1: " + Thread.currentThread().getName());
-                    synchronized (lock2) {
-                        System.out.println("lock2: " + Thread.currentThread().getName());
-                    }
-                }
+                holder(lock1, lock2);
             }
         });
         Thread t2 = new Thread(() -> {
             while (true) {
-                synchronized (lock2) {
-                    System.out.println("lock2: " + Thread.currentThread().getName());
-                    synchronized (lock1) {
-                        System.out.println("lock1: " + Thread.currentThread().getName());
-                    }
-                }
+                holder(lock2, lock1);
             }
         });
 
         t1.start();
         t2.start();
+    }
+
+    static void holder(String lock1, String lock2) {
+        System.out.println(Thread.currentThread().getName() + " Waiting " + lock1);
+        synchronized (lock1) {
+            System.out.println(Thread.currentThread().getName() + " Holding " + lock1);
+            System.out.println(Thread.currentThread().getName() + " Waiting " + lock2);
+            synchronized (lock2) {
+                System.out.println(Thread.currentThread().getName() + " Holding " + lock2);
+            }
+        }
     }
 }
