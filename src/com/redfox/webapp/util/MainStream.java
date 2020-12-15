8dcade3659eb550a0b81696a0ea5ad8bd8802375
Vladislav Lisin <v1adis1av.lis@mail.ru>
2020-12-15 09:07:01 +0300

    added new features

---
+++ src/com/redfox/webapp/util/MainStream.java
@@ -0,0 +1,61 @@
+package com.redfox.webapp.util;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MainStream {
+
+    public static void main(String[] args) {
+//        int[] values = new int[]{1, 2, 3, 3, 2, 3};
+        int[] values = new int[]{8, 9};
+        System.out.println(minValue(values));
+        //
+        System.out.println("===================================");
+        //
+        System.out.println(oddOrEven(Arrays.asList(1, 2, 3, 4, 5)));
+    }
+
+    /*
+        Метод принимает массив цифр от 1 до 9, надо выбрать уникальные и вернуть минимально возможное число,
+        составленное из этих уникальных цифр. Например {1,2,3,3,2,3} вернет 123, а {9,8} вернет 89
+     */
+    static int minValue(int[] values) {
+        values = Arrays.stream(values).
+                filter(value -> 0 < value && value < 10).distinct().sorted().toArray();
+
+        return getSum(values);
+    }
+
+    private static int getSum(int[] values) {
+        int counter = values.length - 1;
+        int sum = 0;
+        for (int i = 0; i < values.length; i++) {
+            sum = (sum + (values[i] * (int) Math.pow(10, counter)));
+            counter--;
+        }
+        return sum;
+    }
+
+    /*
+        Метод принимает список цифр и если сумма всех чисел нечетная - удалить все нечетные, если четная - удалить все четные.
+        Сложность алгоритма O(N).
+     */
+    static List<Integer> oddOrEven(List<Integer> integers) {
+//        System.out.println("Sum = " + integers.stream()
+//                .reduce((acc, x) -> acc + x)
+//                .get());
+        return Stream.of(integers.stream()
+                .reduce((acc, x) -> acc + x)
+                .get())
+                .flatMap((Function<Integer, Stream<Integer>>) x -> {
+                    if (x % 2 == 0) {
+                        return integers.stream().filter(e -> e % 2 != 0);
+                    } else {
+                        return integers.stream().filter(o -> o % 2 == 0);
+                    }
+                }).collect(Collectors.toList());
+    }
+}
