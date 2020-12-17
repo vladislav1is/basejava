598b72033652a7a824b1a44d76f8926c6388f439
Vladislav Lisin <v1adis1av.lis@mail.ru>
2020-12-17 15:50:01 +0300

    code refactoring

--- src/com/redfox/webapp/MainStream.java
+++ src/com/redfox/webapp/MainStream.java
@@ -7,12 +7,9 @@ import java.util.stream.Collectors;
 public class MainStream {
 
     public static void main(String[] args) {
-        int[] values = new int[]{1, 2, 3, 3, 2, 3};
-//        int[] values = new int[]{8, 9};
+//        int[] values = new int[]{1, 2, 3, 3, 2, 3};
+        int[] values = new int[]{8, 9};
         System.out.println(minValue(values));
-        //
-        System.out.println("===================================");
-        //
         System.out.println(oddOrEven(Arrays.asList(1, 2, 3, 4, 5)));
     }
 
@@ -21,21 +18,25 @@ public class MainStream {
         составленное из этих уникальных цифр. Например {1,2,3,3,2,3} вернет 123, а {9,8} вернет 89
      */
     static int minValue(int[] values) {
-        return Arrays.stream(values).distinct().sorted()
-                .reduce((left, right) -> left * 10 + right).getAsInt();
+        return Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .reduce(0, (left, right) -> left * 10 + right);
     }
 
     /*
-        Метод принимает список цифр и если сумма всех чисел нечетная - удалить все нечетные, если четная - удалить все четные.
-        Сложность алгоритма O(N).
+        Метод принимает список цифр и если сумма всех чисел нечетная - удалить все нечетные,
+        если четная - удалить все четные.
      */
     static List<Integer> oddOrEven(List<Integer> integers) {
-//        System.out.println("Sum = " + integers.stream()
-//                .reduce((acc, x) -> acc + x)
-//                .get());
-        Integer sum = integers.stream()
-                .reduce((acc, x) -> acc + x)
-                .get();
-        return integers.stream().filter(x -> (sum % 2 == 0) == (x % 2 != 0)).collect(Collectors.toList());
+        final int sum = integers.stream()
+                .reduce(0, Integer::sum);
+        final boolean isEven = sum % 2 == 0;
+        return integers.stream()
+                .filter(x -> {
+                    int num = x % 2;
+                    return isEven == (num != 0);
+                })
+                .collect(Collectors.toList());
     }
 }
